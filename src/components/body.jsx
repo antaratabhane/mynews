c0fe7bb0de6c2f367126d0d9d566275a8e0599f9
antaratabhane <artofank@gmail.com>
2026-04-21 19:27:14 +0530

    fixed card layout

--- src/components/body.jsx
+++ src/components/body.jsx
@@ -4,7 +4,7 @@ const Body = () => {
   return (
     <div className='bg-linear-to-b from-[#e9eccf] via-[#e2e6b8] to-[#c9d87a] flex flex-col items-center py-8'>
         <div className='my-4 bg-[#e6e8c8] h-60 w-2/3 rounded-2xl flex flex-wrap shadow-[6px_6px_12px_#c4ce8a,-6px_-6px_12px_#ffffff]'>
-            <img src="https://cdn.pixabay.com/photo/2016/02/01/00/56/652234-news-1172463_1280.jpg" className='w-1/3 rounded-l-2xl object-cover'/>
+            <img src="https://cdn.pixabay.com/photo/2016/02/01/00/56/652234-news-1172463_1280.jpg" className='w-1/3 h-full rounded-l-2xl object-cover'/>
             <div className='w-2/3 ps-6 flex flex-col justify-center gap-4'>
                 <h4 className='text-xl font-bold'>Lorem ipsum dolor sit amet.</h4>
                 <div  className='text-xs'>13th April, 2026</div>
@@ -12,7 +12,7 @@ const Body = () => {
             </div>
         </div>
         <div className='my-4 bg-[#e6e8c8] h-60 w-2/3 rounded-2xl flex flex-wrap shadow-[6px_6px_12px_#c4ce8a,-6px_-6px_12px_#ffffff]'>
-            <img src="https://cdn.pixabay.com/photo/2016/02/01/00/56/652234-news-1172463_1280.jpg" className='w-1/3 rounded-l-2xl object-cover'/>
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVdJA1ESMgqXMEYIhZ6DLOzzUevKboO7skDA&s" className='w-1/3 h-full rounded-l-2xl object-cover'/>
             <div className='w-2/3 ps-6 flex flex-col justify-center gap-4'>
                 <h4 className='text-xl font-bold'>Lorem ipsum dolor sit amet.</h4>
                 <div  className='text-xs'>13th April, 2026</div>
@@ -20,7 +20,7 @@ const Body = () => {
             </div>
         </div>
         <div className='my-4 bg-[#e6e8c8] h-60 w-2/3 rounded-2xl flex flex-wrap shadow-[6px_6px_12px_#c4ce8a,-6px_-6px_12px_#ffffff]'>
-            <img src="https://cdn.pixabay.com/photo/2016/02/01/00/56/652234-news-1172463_1280.jpg" className='w-1/3 rounded-l-2xl object-cover'/>
+            <img src="https://cdn.pixabay.com/photo/2016/02/01/00/56/652234-news-1172463_1280.jpg" className='w-1/3 h-full rounded-l-2xl object-cover'/>
             <div className='w-2/3 ps-6 flex flex-col justify-center gap-4'>
                 <h4 className='text-xl font-bold'>Lorem ipsum dolor sit amet.</h4>
                 <div  className='text-xs'>13th April, 2026</div>
@@ -28,7 +28,7 @@ const Body = () => {
             </div>
         </div>
         <div className='my-4 bg-[#e6e8c8] h-60 w-2/3 rounded-2xl flex flex-wrap shadow-[6px_6px_12px_#c4ce8a,-6px_-6px_12px_#ffffff]'>
-            <img src="https://cdn.pixabay.com/photo/2016/02/01/00/56/652234-news-1172463_1280.jpg" className='w-1/3 rounded-l-2xl object-cover'/>
+            <img src="https://cdn.pixabay.com/photo/2016/02/01/00/56/652234-news-1172463_1280.jpg" className='w-1/3 h-full rounded-l-2xl object-cover'/>
             <div className='w-2/3 ps-6 flex flex-col justify-center gap-4'>
                 <h4 className='text-xl font-bold'>Lorem ipsum dolor sit amet.</h4>
                 <div  className='text-xs'>13th April, 2026</div>
@@ -36,7 +36,7 @@ const Body = () => {
             </div>
         </div>
         <div className='my-4 bg-[#e6e8c8] h-60 w-2/3 rounded-2xl flex flex-wrap shadow-[6px_6px_12px_#c4ce8a,-6px_-6px_12px_#ffffff]'>
-            <img src="https://cdn.pixabay.com/photo/2016/02/01/00/56/652234-news-1172463_1280.jpg" className='w-1/3 rounded-l-2xl object-cover'/>
+            <img src="https://cdn.pixabay.com/photo/2016/02/01/00/56/652234-news-1172463_1280.jpg" className='w-1/3 h-full rounded-l-2xl object-cover'/>
             <div className='w-2/3 ps-6 flex flex-col justify-center gap-4'>
                 <h4 className='text-xl font-bold'>Lorem ipsum dolor sit amet.</h4>
                 <div  className='text-xs'>13th April, 2026</div>
