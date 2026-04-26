a40924cb28aef8a2f5b9b89db41269312c23df00
antaratabhane <artofank@gmail.com>
2026-04-26 23:36:29 +0530

    api changed

--- src/components/body.jsx
+++ src/components/body.jsx
@@ -9,14 +9,14 @@ const Body = ({category, search, page, setPage, lang}) => {
     const yesterday = new Date()
     yesterday.setDate(yesterday.getDate() - 1)
     const date = yesterday.toISOString().split("T")[0]
-    const totalPages = Math.ceil(articles.length / 8);
+    const totalPages = Math.ceil((articles?.length || 0) / 8);
 
 
     useEffect(() => {
         const fetchNews = async() => {
-            const response = await fetch(`https://newsapi.org/v2/everything?q=${search || category}&from=${date}&sortBy=popularity&language=en&apiKey=${apikey}`)
+            const response = await fetch(`https://api.currentsapi.services/v1/search?keywords=${search || category}&language=en&apiKey=${apikey}`)
             let data = await response.json()
-            setArticles(data.articles)
+            setArticles(data.news || [])
         }
         fetchNews();
     }, [category, search])
@@ -38,7 +38,7 @@ const Body = ({category, search, page, setPage, lang}) => {
     <div className='min-h-screen bg-linear-to-r from-[#e9eccf] via-[#e2e6b8] to-[#c9d87a] flex flex-col items-center py-8'>
         {translatedArticle.slice((page - 1) * 8, page * 8).map((news, index) => (
             <div key={index} className='my-4 bg-[#e6e8c8] min-h-60 w-2/3 rounded-2xl flex flex-wrap shadow-[6px_6px_12px_#c4ce8a,-6px_-6px_12px_#ffffff]'>
-                <img src={news.urlToImage} className='rounded-t-2xl w-full md:w-1/3 md:h-auto md:rounded-l-2xl md:rounded-r-none md:object-cover'
+                <img src={news.image} className='rounded-t-2xl w-full md:w-1/3 md:h-auto md:rounded-l-2xl md:rounded-r-none md:object-cover'
                     onError={(e) => {
                         e.target.onerror = null;
                         e.target.src = "https://thumbs.dreamstime.com/b/news-woodn-dice-depicting-letters-bundle-small-newspapers-leaning-left-dice-34802664.jpg";
